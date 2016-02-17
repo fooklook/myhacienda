d77b19fdfc80be27878f76a24c5ab263342cc9c3
fooklook <xiashuo.he@foxmail.com>
2016-02-17 18:26:58 +0800

    webhooks test1

--- app/Http/Controllers/HookController.php
+++ app/Http/Controllers/HookController.php
@@ -1,17 +1,20 @@
 <?php namespace App\Http\Controllers;
 
+use App\Hook;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Input;
 
 class HookController extends Controller {
 
 	public function storeEvents(Request $request) {
-		echo $event_name = $request->header('X-Github-Event');
+		$event_name = $request->header('X-Github-Event');
 		$body = json_encode(Input::all());
+		var_dump($body);
 
-		$hook = new Hook;
+		$hook = new Hook();
 		$hook->event_name = $event_name;
 		$hook->payload = $body;
 
