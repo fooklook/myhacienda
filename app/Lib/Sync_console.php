6c0daf7b3b3539e13ac14113571b5b75a90aefa1
fooklook <xiashuo.he@foxmail.com>
2016-02-27 14:56:27 +0800

    排除错误

--- app/Lib/Sync_console.php
+++ app/Lib/Sync_console.php
@@ -98,25 +98,16 @@ class Sync_console
             }
             //新增
             foreach ($commit["added"] AS $addad){
-                if($addad == "master"){
-                    continue;
-                }
                 $sync_file = Sync_file::instantiate($addad,$this->user);
                 $sync_file->added($addad);
             }
             //删除
             foreach ($commit["removed"] AS $removed){
-                if($removed == "master"){
-                    continue;
-                }
                 $sync_file = Sync_file::instantiate($removed,$this->user);
                 $sync_file->removed($removed);
             }
             //修改
             foreach ($commit["modified"] AS $modified){
-                if($modified == "master"){
-                    continue;
-                }
                 $sync_file = Sync_file::instantiate($modified,$this->user);
                 $sync_file->modified($modified);
             }
