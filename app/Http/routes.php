297cfec878348a346228d50a96f05f60aa217b63
fooklook <xiashuo.he@foxmail.com>
2016-03-16 18:14:38 +0800

    robots文件设置

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -3,21 +3,23 @@
 Route::get("/", "HomeController@index");
 //博客
 Route::group(['prefix' => 'blog'],function(){
+
 	//列表页
 	Route::get('{classify}', 'BlogController@listpage');
 	//详细页
 	Route::get('{classify}/{article}', 'BlogController@detailpage');
+
 });
 /** 登录注册功能 **/
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
-Route::get('create', 'HomeController@create');
-Route::get('error404',function(){
-	return view('errors.404');
-});
-Route::post("push","HookController@storeEvents");
-Route::get('email',function(){
-	\Illuminate\Support\Facades\Queue::push(new \App\Commands\SendEmail('1013149199@qq.com'));
-});
+//Route::controllers([
+//	'auth' => 'Auth\AuthController',
+//	'password' => 'Auth\PasswordController',
+//]);
+//Route::get('create', 'HomeController@create');
+//Route::get('error404',function(){
+//	return view('errors.404');
+//});
+//Route::post("push","HookController@storeEvents");
+//Route::get('email',function(){
+//	\Illuminate\Support\Facades\Queue::push(new \App\Commands\SendEmail('1013149199@qq.com'));
+//});
